0f34642bd5ce33d72c0b0604c39b0de84f46fa77
AlgorithmHaGadol <1337softwaredevelopment@gmail.com>
2025-01-03 15:25:08 +0200

    setting up initial structure
    finished setting up the structure
    added boiler plate code for most things

--- includes/config.php
+++ includes/config.php
@@ -1,2 +1,17 @@
 <?php
+session_start();
+$env = parse_ini_file('.env');
+$servername = 'localhost';
+$username = $env['DB_USERNAME'];
+$password = $env['DB_PASSWORD'];
+$dbname = $env['DB_NAME'];
+
 $isUpdating = false;
+
+class dbConnection extends mysqli {
+}
+
+$conn = new dbConnection($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php include 'includes/header.php'; ?>
+<?php include 'includes/header.php'; 
+if ($isUpdating) {
+  include_once('updating.html');
+  return;
+}
+?>
+
+<h1>HOME PAGE</h1>
+
+<?php include 'includes/footer.php';?>
